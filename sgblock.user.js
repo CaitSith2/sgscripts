58132e312290b369002aa6bf57f40228225c8ed0
Daerphen <johnny@parallel-bits.com>
2016-01-27 22:24:03 +0100

    created script
     to notify on giveaway page if game is blocked

---
+++ sgblock.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         SG Blocked Games
+// @namespace    com.parallelbits
+// @version      0.1
+// @description  Notify within a giveaway if you blocked it
+// @author       Daerphen
+// @match        http://www.steamgifts.com/giveaway/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+function _blockGame(context) {
+    var hit = 0;
+    $(context).find('div.table__rows p.table__column__heading').each(function(i, value) {
+        hit++;
+    });
+    if(hit > 0) {
+        var item = $('div.sidebar__entry-insert')
+        item.removeClass('sidebar__entry-insert').addClass('sidebar__error');
+        var text = item.text();
+        item.text(text.replace(/Enter Giveaway/g, "Giveaway blocked "));
+    }
+}
+
+var name = $('div.featured__heading__medium').text();
+
+var page = "http://www.steamgifts.com/account/settings/giveaways/filters/search?q=";
+name = name.replace(/ /g, "%20");
+var uri = page + name;
+$.ajax(uri, {
+  async: true
+}).done(function(context) {
+    _blockGame(context);
+});
