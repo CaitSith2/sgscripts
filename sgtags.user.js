b59d84a69893fb2d1a25413c3d6dee8644763d08
Daerphen <johnny@parallel-bits.com>
2016-01-28 19:45:49 +0100

    update version

--- sgtags.user.js
+++ sgtags.user.js
@@ -3,7 +3,7 @@
 // @namespace   com.parallelbits
 // @description Adds genre tags to game list
 // @include     http://www.steamgifts.com/*
-// @version     0.1
+// @version     0.2
 // @grant       none
 // ==/UserScript==
 
