bafe627809eaa85c4274a23da4e8e11d8c9fab2c
Daerphen <johnny@parallel-bits.com>
2016-03-02 11:23:11 +0100

    adapted to proxy changes

--- sgtags.user.js
+++ sgtags.user.js
@@ -3,7 +3,7 @@
 // @namespace   com.parallelbits
 // @description Adds genre tags to game list
 // @include     http://www.steamgifts.com/*
-// @version     0.21
+// @version     1.01
 // @grant       none
 // ==/UserScript==
 
@@ -11,24 +11,32 @@ var genre = [];
 var PROXY_URL = 'http://proxy.parallel-bits.com/proxy';
 
 function _addTags(uri, key) {
-  var item = $('h2.giveaway__heading a[href="'+uri+'"]').parent();
-  item.find('span.gametag').replaceWith('');
-  genre[key].forEach(function(value) {
-    item.append('<span class="gametag" style="margin-left: 10px; padding: 2px; color: gray; border: 1px solid rgba(0,0,0,.1); border-radius: 4px">'+value.description+'</span>');
-  });
+	var item = $('h2.giveaway__heading a[href="' + uri + '"]').parent();
+	item.find('span.gametag').replaceWith('');
+	genre[key].forEach(function(value) {
+		item.append('<span class="gametag" style="margin-left: 10px; padding: 2px; color: gray; border: 1px solid rgba(0,0,0,.1); border-radius: 4px">' + value.description + '</span>');
+	});
 }
 
 $('a.giveaway__icon').each(function(i, value) {
-  var link = $(this).attr("href");
-  var reg = /(?:\/(app)\/([0-9]+)\/)|(?:(sub)\/([0-9]+)\/)/g;
-  var data = reg.exec(link);
-  if(data[1] === 'app') {
-      var appURI = PROXY_URL + '?uri=app/' + data[2];
-      $.ajax(appURI, {
-        async: true
-      }).done(function(context) {
-        genre['app/'+data[2]] = context[data[2]].data.genres;
-        _addTags(value, 'app/'+data[2]);
-      });
-  } 
+	var link = $(this).attr("href");
+	var reg = /(?:\/(app)\/([0-9]+)\/)|(?:(sub)\/([0-9]+)\/)/g;
+	var data = reg.exec(link);
+	if (data[1] === 'app') {
+		var appURI = PROXY_URL + '?uri=app/' + data[2];
+		$.ajax(appURI, {
+			async: true
+		}).done(function(context) {
+			genre['app/' + data[2]] = context.genres;
+			_addTags(value, 'app/' + data[2]);
+		});
+	} else if (data[1] === 'sub') {
+		var subURI = PROXY_URL + '?uri=sub/' + data[2];
+		$.ajax(subURI, {
+			async: true
+		}).done(function(context) {
+			genre['sub/' + data[2]] = context.genres;
+			_addTags(value, 'sub/' + data[2]);
+		});
+	}
 });
