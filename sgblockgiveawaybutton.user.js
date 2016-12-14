984ba5c854b615087f2c01fdb2754fe08ff5fbc6
Daerphen <johnny@parallel-bits.com>
2016-12-14 08:52:27 +0100

    removed hide button. Not needed anymore, because supplied by steamgifts

--- sgblockgiveawaybutton.user.js
+++ sgblockgiveawaybutton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SG Block Giveaway Button
 // @namespace    com.parallelbits
-// @version      1.12
+// @version      1.13
 // @description  Add a Button to Giveawaypage to block the game
 // @author       Daerphen
 // @match        *://www.steamgifts.com/giveaway/*
@@ -10,110 +10,42 @@
 /* jshint -W097 */
 'use strict';
 
-let game_name = $('title').text();
-let uri = $('a.global__image-outer-wrap--game-large').attr('href');
-let XSRF = null;
-let popup = null;
-let game_id = null;
-let hide_button = null;
-let status = false;
-let enter_button_text = null;
+var game_name = $('title').text();
+var uri = $('a.global__image-outer-wrap--game-large').attr('href');
+var XSRF = null;
+var popup = null;
+var game_id = null;
+var hide_button = null;
+var status = false;
+var enter_button_text = null;
 
 function _addButton() {
-    if(XSRF !== null && hide_button === null) {
-        hide_button = '<h3 class="sidebar__heading">Giveaway Filters</h3><ul class="sidebar__navigation">' + 
-            '<li id="hide-header" class="sidebar__navigation__item">' + 
-            '' + 
-            '</li></ul>';
-        
-        let sidebar = $('div[class="sidebar sidebar--wide"]');
-        sidebar.append(hide_button);
-        $('#hide-header').append('<a id="hide-game" href="javascript:void(0)" class="sidebar__navigation__item__link"><div class="sidebar__navigation__item__name">Hide Game</div></a>');
-        $('#hide-header').append('<a id="unhide-game" href="javascript:void(0)" class="sidebar__navigation__item__link"><div class="sidebar__navigation__item__name">Unhide Game</div></a>');
-        console.log(status);
-        if(status) {
-            $('#hide-game').addClass('is-hidden');
-        } else {
-            $('#unhide-game').addClass('is-hidden');
-        }
-		_updateButton();
-        $('#hide-game').on('click', function() {
-            let sink = 'https://www.steamgifts.com/ajax.php';
-            let data = {
-                "xsrf_token": XSRF,
-                "do": "hide_giveaways_by_game_id",
-                "game_id": game_id
-            };
-            $.ajax(sink, {
-                data: data,
-                type: "POST"
-            }).done(function() {
-                status = true;
-                _updateButton();
-            });
-        });
-        $('#unhide-game').on('click', function() {
-            let sink = 'https://www.steamgifts.com/ajax.php';
-            let data = {
-                "xsrf_token": XSRF,
-                "do": "remove_filter",
-                "game_id": game_id
-            };
-            $.ajax(sink, {
-                data: data,
-                type: "POST"
-            }).done(function() {
-                status = false;
-                _updateButton();
-            });
-        });
-    }
+    var sidebar = $('div[class="sidebar sidebar--wide"]');
+    _updateButton();
 }
 
 function _updateButton() {
-    let buttonText = enter_button_text;
+    var buttonText = enter_button_text;
     
     
     if(status) {
-        $('#hide-game').addClass('is-hidden');
-        $('#unhide-game').removeClass('is-hidden');
-        buttonText = buttonText.replace(/Enter Giveaway/g, 'Giveaway blocked');
-        let enterButton = $('div.sidebar__entry-insert');
-        if(enterButton.length) {
+       buttonText = buttonText.replace(/Enter Giveaway/g, 'Giveaway blocked');
+       var enterButton = $('div.sidebar__entry-insert');
+       if(enterButton.length) {
             enterButton.addClass('sidebar__error');
             enterButton.html(buttonText);
-        }
+       }
     } else {
-        $('#hide-game').removeClass('is-hidden');
-        $('#unhide-game').addClass('is-hidden');
-        let enterButton = $('div.sidebar__error');
+        var enterButton = $('div.sidebar__error');
         if(enterButton.length && enterButton.hasClass('sidebar__entry-insert')) {
             enterButton.removeClass('sidebar__error');
             enterButton.html(buttonText);
         }
     }
-    
-    
-    
-}
-
-function _setXSRF(context) {
-    popup = $(context);
-    XSRF = popup.find('input[name="xsrf_token"]').attr('value');
-    _addButton();
-}
-
-function _setGameId(context) {
-    let data = JSON.parse(context);
-    $(data.html).find('a.table__column__secondary-link').each(function(index, value) {
-        if($(value).attr('href') === uri) {
-            game_id = $(value).parent().parent().parent().parent().attr('data-autocomplete-id');
-        }
-    });
 }
 
 function _setStatus(context) {
-    let hit = 0;
+    var hit = 0;
     $(context).find('div.table__rows p a.table__column__secondary-link').each(function(i, value) {
         if($(value).attr('href') === uri) {
             hit++;
@@ -134,31 +66,10 @@ $.ajaxSetup({
   }
 });
 
-
-
-let gameFetchURL = 'https://www.steamgifts.com/ajax.php';
-let searchData = {
-    'do': 'autocomplete_game',
-    'search_query': game_name.replace(/ /g, '+'),
-    'page_number': 1
-}
-$.ajax(gameFetchURL, {
-    async: true,
-    method: 'post',
-    data: searchData
-}).done(function(context) {
-    _setGameId(context);
-    let popupURL = 'https://www.steamgifts.com';
-    $.ajax(popupURL, {
-        async: true
-    }).done(function(context) {
-        _setXSRF(context);
-    });
-});
-
-let statusURL = 'https://www.steamgifts.com/account/settings/giveaways/filters/search?q=' + game_name.replace(/ /g, '+');
+var statusURL = 'https://www.steamgifts.com/account/settings/giveaways/filters/search?q=' + game_name.replace(/ /g, '+');
 $.ajax(statusURL).done(function(context) {
     _setStatus(context);
+    _updateButton();
 });
         
         
